b87416ea9440397c52e48173361afdb92946bbd2
machengyi <920815733@qq.com>
2016-09-06 11:20:59 +0800

    one

---
+++ www/js/app.js
@@ -0,0 +1,68 @@
+angular.module('App', ['ionic'])
+    .config(function ($stateProvider, $urlRouterProvider) {
+        $stateProvider
+        .state('tabs', {
+            url: '/tabs',
+            abstract: true,
+            templateUrl: 'views/tabs/tabs.html'
+        })
+        .state('tabs.rates', {
+            url: '/rates',
+            views: {
+                'rates-tab': {
+                    templateUrl: 'views/rates/rates.html',
+                    controller: 'RatesController'
+                }
+            }
+        })
+        .state('tabs.history', {
+            url: '/history',
+            views: {
+                'history-tab': {
+                    templateUrl: 'views/history/history.html'
+                }
+            }
+        })
+        .state('tabs.currencies', {
+            url: '/currencies',
+            views: {
+                'currencies-tab': {
+                    templateUrl: 'views/currencies/currencies.html'
+                }
+            }
+        });
+        $urlRouterProvider.otherwise('/tabs/rates');
+    })
+
+.run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+        if (window.StatusBar) {
+            StatusBar.styleDefault();
+        }
+    });
+})
+
+.factory('Currencies', function () {
+    return [
+        { code: 'AUD', text: 'Australian Dollar', selected: true },
+        { code: 'BRL', text: 'Brazilian Real', selected: true },
+        { code: 'CAD', text: 'CanaDan Dollar', selected: true },
+        { code: 'EUR', text: 'Chiness Yuan', selected: true },
+        { code: 'GBP', text: 'Euro Dollar', selected: true },
+        { code: 'IDR', text: 'British Pound Sterling', selected: true },
+        { code: 'ILS', text: 'Indonesian Rupiah', selected: true },
+        { code: 'NOK', text: 'Polish Zloty', selected: true },
+        { code: 'NZD', text: 'Romanian Leu', selected: false },
+
+        { code: 'PLN', text: 'Polish Zloty', selected: true },
+        { code: 'RON', text: 'Romanian Real', selected: true },
+        { code: 'RUB', text: 'Russian Ruble', selected: true },
+        { code: 'SEK', text: 'Swedish Krona', selected: true },
+        { code: 'SGD', text: 'Singapore Dollar', selected: true },
+        { code: 'USD', text: 'United States Dollar', selected: true },
+        { code: 'ZAR', text: 'South African Rand', selected: true }
+    ];
+});
